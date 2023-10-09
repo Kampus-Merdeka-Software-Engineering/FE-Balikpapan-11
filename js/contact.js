c3b2c250408a08e970ac264e493433a0bdfb669a
etgeriflimba <21104030@ittelkom-pwt.ac.id>
2023-10-09 22:58:49 +0700

    update

--- js/contact.js
+++ js/contact.js
@@ -1,29 +1,33 @@
-const form = document.querySelector('#form');
-const inputName = document.querySelector('#input_name');
-const email = document.querySelector('#email');
-const number = document.querySelector('#number');
-const message = document.querySelector('#message');
-const sendBtn = document.querySelector('#send');
+const form = document.querySelector("#form");
+const inputName = document.querySelector("#input_name");
+const email = document.querySelector("#email");
+const number = document.querySelector("#number");
+const message = document.querySelector("#message");
+const sendBtn = document.querySelector("#send");
 
-sendBtn.addEventListener('click', (event) => {
-event.preventDefault();
+sendBtn.addEventListener("click", (event) => {
+  event.preventDefault();
 
-const data = {
+  const data = {
+    name: inputName.value,
+    email: email.value,
+    number: number.value,
+    message: message.value,
+  };
 
-name: inputName.value,
-email: email.value,
-number: number.value,
-message: message.value
-};
-
-fetch('https://be-balikpapan-11-production-223d.up.railway.app/contactus', {
-method: 'POST',
-headers: {
-  'Content-Type': 'application/json'
-},
-body: JSON.stringify(data)
-})
-.then(response => response.json())
-.then(data => console.log(data))
-.catch(error => console.error(error));
+  fetch("https://be-balikpapan-11-production-223d.up.railway.app/contactus", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      alert("Terima kasih telah menghubungi kami");
+      // reset form
+      form.reset();
+      console.log(data);
+    })
+    .catch((error) => console.error(error));
 });
